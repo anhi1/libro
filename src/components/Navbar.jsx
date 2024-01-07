2aa80b8699210b12c83fdcc16fdfcd8e7a6c4181
anhi1 <anhiemolina@gmail.com>
2024-01-07 12:41:20 +0100

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 
 import Link from "next/link";
+import Image from "next/image";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container">
         <Link className="navbar-brand" href="/">
-          logo
+          <img className="logo" alt="Logo"  src="./logo.svg"/>
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
