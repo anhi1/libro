59a4b17768da2bd1e32dabb31a18f372e0140cc8
Ocho93 <ar.sanchez.p93@gmail.com>
2024-01-08 13:59:24 +0100

    login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,127 +1,98 @@
-export default function Login() {
+import Head from 'next/head';
+
+const LoginPage = () => {
   return (
     <>
-      <div className="flex items-center min-h-screen p-4 bg-gray-100 lg:justify-center">
-        <div className="flex flex-col overflow-hidden bg-white rounded-md shadow-lg max md:flex-row md:flex-1 lg:max-w-screen-md">
-          <div className="p-4 py-6 bg-blue-500 text-white md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:justify-evenly">
-            <img
-              src="/img/LogosinFondo.svg"
-              alt="Logo"
-              className="w-32 h-auto"
-            />
-          </div>
-          <div className="p-5 bg-white md:flex-1">
-            <h3 className="my-4 text-2xl font-semibold text-gray-700">
-              Inicio de sesión
-            </h3>
-            <form action="#" className="flex flex-col space-y-5">
-              <div className="flex flex-col space-y-1">
-                <label
-                  htmlFor="email"
-                  className="text-sm font-semibold text-gray-500"
-                >
-                  Correo electrónico
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  autoFocus
-                  className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
-                />
-              </div>
-              <div className="flex flex-col space-y-1">
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="text-sm font-semibold text-gray-500"
-                  >
+      <Head>
+        <title>Inicio de sesión</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        />
+      </Head>
+      <section className="vh-100 d-flex justify-content-center align-items-center">
+        <div className="container-fluid h-custom">
+          <div className="row">
+            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1 d-flex align-items-center">
+              <form className="w-100">
+              <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                  <p className="lead fw-normal mb-0 me-3">Acceder con:</p>
+                  <button type="button" className="btn btn-primary btn-floating mx-1">
+                    <i className="fab fa-facebook-f"></i>
+                  </button>
+
+                  <button type="button" className="btn btn-primary btn-floating mx-1">
+                    <i className="fab fa-twitter"></i>
+                  </button>
+
+                  <button type="button" className="btn btn-primary btn-floating mx-1">
+                    <i className="fab fa-linkedin-in"></i>
+                  </button>
+                </div>
+
+                {/* Email input */}
+                <div className="form-outline mb-4">
+                  <input
+                    type="email"
+                    id="form3Example3"
+                    className="form-control form-control-lg"
+                    placeholder="Introduce un correo"
+                  />
+                  <label className="form-label" htmlFor="form3Example3">
+                    Correo electrónico
+                  </label>
+                </div>
+
+                {/* Password input */}
+                <div className="form-outline mb-3">
+                  <input
+                    type="password"
+                    id="form3Example4"
+                    className="form-control form-control-lg"
+                    placeholder="Introduce una contraseña"
+                  />
+                  <label className="form-label" htmlFor="form3Example4">
                     Contraseña
                   </label>
-                  <a
-                    href="#"
-                    className="text-sm text-blue-600 hover:underline focus:text-blue-800"
-                  >
+                </div>
+
+                <div className="d-flex justify-content-between align-items-center">
+                  {/* Checkbox */}
+                  <div className="form-check mb-0">
+                    <input
+                      className="form-check-input me-2"
+                      type="checkbox"
+                      value=""
+                      id="form2Example3"
+                    />
+                    <label className="form-check-label" htmlFor="form2Example3">
+                      Recuérdame
+                    </label>
+                  </div>
+                  <a href="#!" className="text-body">
                     ¿Has olvidado tu contraseña?
                   </a>
                 </div>
-                <input
-                  type="password"
-                  id="password"
-                  className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
-                />
-              </div>
-              <div className="flex items-center space-x-2">
-                <input
-                  type="checkbox"
-                  id="remember"
-                  className="w-4 h-4 transition duration-300 rounded focus:ring-2 focus:ring-offset-0 focus:outline-none focus:ring-blue-200"
-                />
-                <label
-                  htmlFor="remember"
-                  className="text-sm font-semibold text-gray-500"
-                >
-                  Recuérdame
-                </label>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  className="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
-                >
-                  Iniciar sesión
-                </button>
-              </div>
-              <div className="flex flex-col space-y-5">
-                <span className="flex items-center justify-center space-x-2">
-                  <span className="h-px bg-gray-400 w-14"></span>
-                  <span className="font-normal text-gray-500">
-                    o acceder con:
-                  </span>
-                  <span className="h-px bg-gray-400 w-14"></span>
-                </span>
-                <div className="flex flex-col space-y-4">
-                  <a
-                    href="#"
-                    className="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-gray-800 rounded-md group hover:bg-gray-800 focus:outline-none"
+
+                <div className="text-center text-lg-start mt-4 pt-2">
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-lg"
+                    style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }}
                   >
-                    <span>
-                      <svg
-                        className="w-5 h-5 text-gray-800 fill-current group-hover:text-white"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-                        ></path>
-                      </svg>
-                    </span>
-                    <span className="text-sm font-medium text-gray-800 group-hover:text-white">
-                      Github
-                    </span>
-                  </a>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-gray-800 rounded-md group hover:bg-gray-800 focus:outline-none"
-                  >
-                    <span>
-                      <img
-                        className="w-5 h-5"
-                        src="/img/spotify.png"
-                        alt="Spotify Icon"
-                      />
-                    </span>
-                    <span className="text-sm font-medium text-gray-800 group-hover:text-white">
-                      Spotify
-                    </span>
-                  </a>
+                    Iniciar sesión
+                  </button>
+                  <p className="small fw-bold mt-2 pt-1 mb-0">
+                    ¿Aún no tienes cuenta? <a href="#!" className="link-danger">Regístrate</a>
+                  </p>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
-}
+};
+
+export default LoginPage;
