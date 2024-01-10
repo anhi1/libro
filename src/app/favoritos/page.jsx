6de5effcb6d48795a7f9e62a983bd67f2509f604
Ocho93 <ar.sanchez.p93@gmail.com>
2024-01-10 11:03:38 +0100

    favoritos

--- src/app/favoritos/page.jsx
+++ src/app/favoritos/page.jsx
@@ -1,10 +1,31 @@
+// src/app/favoritos/page.jsx
 
+import React from 'react';
+import Favoritos from '../../components/Favoritos';
 
-export default function Favoritos(){
-return(
+const FavoritosPage = ({ favoriteBooks }) => {
+  return (
     <div>
-        <h1>favoritos</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui placeat asperiores officia, incidunt unde molestias repudiandae ea nobis quisquam possimus nisi mollitia quos optio eos. Excepturi natus perspiciatis obcaecati quis.</p>
+      <h1>Tus Libros Favoritos</h1>
+      <Favoritos books={favoriteBooks} />
     </div>
-)
-}
+  );
+};
+
+FavoritosPage.getInitialProps = async () => {
+  try {
+    // Leer el archivo JSON de la carpeta 'public'
+    const res = await fetch('/books.json');
+    const booksData = await res.json();
+
+    // Seleccionar los 5 primeros libros como favoritos
+    const favoriteBooks = booksData.slice(0, 5);
+
+    return { favoriteBooks };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return { favoriteBooks: [] };
+  }
+};
+
+export default FavoritosPage;
