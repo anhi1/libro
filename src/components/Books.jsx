f8fcc139631ee0b29005e4617fb2acb4fd1c6437
anhi1 <anhiemolina@gmail.com>
2024-01-15 10:23:18 +0100

    libro

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,49 +1,39 @@
+'use client'
 import { useRouter } from "next/navigation";
 
 export default function Books({ books }) {
+  //propiedad
   const router = useRouter();
-
-  const navigateToBook = (id) => {
-    router.push(`/books/${id}`);
-  };
-
   return (
-    <div>
-      <ul className="list-group">
-        {books.map((book) => (
-          <li
-            key={book.id}
-            className="list-group-item d-flex justify-content-between align-items-center list-group-item-action"
-          >
-            <p>Título: {book.title}</p>
-            <img src={book.photo} alt={book.photo} style={{ width: "100px" }} />
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={() => navigateToBook(book.id)}
-            >
-              Ver Detalles
-            </button>
-          </li>
-        ))}
-      </ul>
-      <nav aria-label="...">
-        <ul className="pagination pagination-lg">
-          <li className="page-item active" aria-current="page">
-            <span className="page-link">1</span>
-          </li>
-          <li className="page-item">
-            <a className="page-link" href="#">
-              2
-            </a>
-          </li>
-          <li className="page-item">
-            <a className="page-link" href="#">
-              3
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <div className="d-flex flex-wrap justify-content-center align-items-center">
+      {books.map((book) => (
+        <div key={book.id} className="card m-3" style={{ width: "400px" }}>
+          <div className="row g-0">
+            <div className="col-md-4 col-12">
+              <img
+                src={book.photo}
+                className="img-fluid rounded-start"
+                alt={book.photo}
+                style={{ height: "200px", objectFit: "cover", width: "100%" }}
+              />
+            </div>
+            <div className="col-md-8 col-12">
+              <div className="card-body">
+                <div className="card-title">
+                  <p><strong>Título: </strong>{book.title}</p>
+                  <button className="btn btn-light"
+                    onClick={() => {
+                      router.push(`/book/${book.id}`);
+                    }}
+                  >
+                    ver
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
